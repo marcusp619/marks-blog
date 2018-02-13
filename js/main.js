747fffe78392aa6155d73b8de56f15425de90525
Mark Pearyer <Marcusp619@gmail.com>
2018-02-13 08:05:56 -0500

    Added local storage

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 let el = document.querySelector("a");
+let form  = document.getElementsByTagName('form')[0];
+let email = document.getElementById('email');
+let button = document.getElementById('theButton');
 
 el.addEventListener("mouseover", ()=> {
   el.innerHTML = 'CLICK ME';
@@ -9,17 +12,20 @@ el.addEventListener("mouseover", ()=> {
 }, false);
 
 
-let form  = document.getElementsByTagName('form')[0];
-let email = document.getElementById('email');
+// email.addEventListener("input", event => {
+//   if (email.validity.valid) {
+//     error.innerHTML = "";
+//   }
+// }, false);
+// form.addEventListener("submit", event => {
+//   if (!email.validity.valid) {
+//     error.innerHTML = "I expect an e-mail";
+//     event.preventDefault();
+//   }
+// }, false);
 
-email.addEventListener("input", event => {
-  if (email.validity.valid) {
-    error.innerHTML = "";
-  }
-}, false);
-form.addEventListener("submit", event => {
-  if (!email.validity.valid) {
-    error.innerHTML = "I expect an e-mail";
-    event.preventDefault();
-  }
-}, false);
+let buttonClick = () => {
+  value = button.form.name.value;
+  // Stores there name in local storage
+  localStorage.setItem(`username: `, `${value}`);
+}
